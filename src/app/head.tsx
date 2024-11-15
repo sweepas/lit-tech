7079e39ef1883fd465cf1221113c853121171224
sweepas <47362063+sweepas@users.noreply.github.com>
2024-11-15 01:36:29 +0000

    .

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <title>Your Page Title</title>
+    </>
+  );
+}
